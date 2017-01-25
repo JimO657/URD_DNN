54a4836e09b482ac878f0ab035369885b78dfb38
kimmmx <max.kimmet@comed.com>
2017-01-25 09:19:07 -0600

    everything's broken

--- DNN_Weather_Predict.py
+++ DNN_Weather_Predict.py
@@ -1,10 +1,10 @@
-# This script does stuff, allegedly
-
 import pandas as pd
 import h2o
 import os
 from h2o import exceptions
 from h2o.estimators.deeplearning import H2ODeepLearningEstimator
+import plotly.plotly as py
+import plotly.graph_objs as go
 
 
 if __name__=='__main__':
@@ -34,8 +34,8 @@ if __name__=='__main__':
     training, validation = train.split_frame(ratios=[0.8])
 
     # Create test data slice and convert to H2OFrame
-    test_pd = data_full[end_row + 1:]#6844]
-    test_pd.drop('Date1', axis=1, inplace=True)
+    test_pd_all = data_full[end_row + 1:]#6844]
+    test_pd = test_pd_all.drop('Date1', 1)
     test = h2o.H2OFrame(test_pd, column_types=['int', 'enum', 'real', 'real', 'int', 'int', 'int', 'int'])
 
     # Define predictors and output
@@ -49,7 +49,7 @@ if __name__=='__main__':
     model.train(x=predictors, y=output, training_frame=training, validation_frame=validation)
 
     # Save DNN model
-    save_path = '~/0MyDataBases/7R/ADHOC_Qlikview-linux/H2O_Models'
+    save_path = '~/0MyDataBases/7R/ADHOC_Qlikview-linux/H2O_Models/'
     try:
         h2o.save_model(model, path=save_path)
     except exceptions.H2OServerError:
@@ -60,10 +60,12 @@ if __name__=='__main__':
     original_prediction = model.predict(test)
 
     # Import weather data to pandas dataframe
-    data_weather = pd.read_csv(
+    data_weather_all = pd.read_csv(
         '~/0MyDataBases/7R/ADHOC_Qlikview-linux/H2O_Models/data_2015/ExportFileWeather_2010.csv')
-    data_weather.drop('Date1', axis=1, inplace=True)
+    data_weather = data_weather_all.drop('Date1', 1)
     test_weather = h2o.H2OFrame(data_weather, column_types=['int', 'enum', 'real', 'real', 'int', 'int', 'int', 'int'])
     weather_prediction = model.predict(test_weather)
 
-    #
+    # Add YearMonth column to dataframes
+    test_pd_all.groupby(pd.TimeGrouper("M")).sum()
+    trace_weather = go.Scatter()
