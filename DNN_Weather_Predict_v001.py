4c6fd4b4cb6c01ab31ba35d0da6a6f9f21c2c954
kimmmx <max.kimmet@comed.com>
2017-02-06 12:29:59 -0600

    Plots yearly aggregation

--- DNN_Weather_Predict_v001.py
+++ DNN_Weather_Predict_v001.py
@@ -49,4 +49,4 @@ if __name__ == "__main__":
         d_years_predictions[l_test_year[i]] = l_pd_test_data[i]
 
     # Plot using external .py file function
-    visualize_urd(data_full, d_years_predictions, aggregations=set(['Daily', 'Yearly', 'Monthly']))
+    visualize_urd(data_full, d_years_predictions, aggregations=set(['Yearly']))
